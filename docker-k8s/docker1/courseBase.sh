0873f2d238ce68106d278441b6c824ec85bb0561
Jmnote <opcore@gmail.com>
2020-08-28 00:09:27 +0900

    Update courseBase.sh

--- docker-k8s/docker1/courseBase.sh
+++ docker-k8s/docker1/courseBase.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rmi -f $(docker images -aq)
-docker volume prune -f
+docker system prune -f
+PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\\$ "
