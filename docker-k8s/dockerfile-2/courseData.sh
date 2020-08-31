6f77b67ec56e15a0d869d8c42055374aaa920335
Jmnote <opcore@gmail.com>
2020-08-31 13:28:48 +0900

    Update courseData.sh

--- docker-k8s/dockerfile-2/courseData.sh
+++ docker-k8s/dockerfile-2/courseData.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+mkdir /root/HelloJava; cd /root/HelloJava
+
+cat <<EOF > HelloJava.java
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
+EOF
+
+cat <<EOF > Dockerfile
+FROM openjdk:8
+COPY HelloJava.java /app/
+WORKDIR /app
+RUN javac HelloJava.java
+CMD ["java","HelloJava"]
+EOF
