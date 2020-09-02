a37b3880e4353c2c0739137e9d76e1e3e3c190db
Jmnote <opcore@gmail.com>
2020-09-03 03:07:18 +0900

    Update 17_nfs-server-manaul.sh

--- k8s/k8s-1/assets/17_nfs-server-manaul.sh
+++ k8s/k8s-1/assets/17_nfs-server-manaul.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-apt-get update -y
-apt install nfs-kernel-server --fix-missing -y
-systemctl enable nfs-server
-systemctl start nfs-server
-systemctl status nfs-kernel-server
-
-mkdir -p /srv/nfs/kubedata
-mkdir /srv/nfs/kubedata/{pv0,pv1,pv2,pv3,pv4}
-chmod -R 777 /srv/nfs
-
-echo "/srv/nfs/kubedata    *(rw,sync,no_subtree_check,insecure)" >> /etc/exports
-exportfs -rav
-exportfs -v
-showmount -e
-
-NFSIP=$(ip addr show ens3  | awk '$1 == "inet" { print $2 }' | cut -d/ -f1)
-echo $NFSIP
-ssh root@node01 mount -t nfs $NFSIP:/srv/nfs/kubedata  /mnt
-ssh root@node01 ls /mnt
+docker run -d --net=host \
+   --privileged --name nfs-server \
+   katacoda/contained-nfs-server:centos7 \
+   /exports/data-0001 /exports/data-0002
