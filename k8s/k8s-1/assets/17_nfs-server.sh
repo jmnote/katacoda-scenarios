3e9e4e0d38f7f0f0ff28eb6f887124d8f8e87b14
Jmnote <opcore@gmail.com>
2020-09-04 02:46:14 +0900

    Update and rename 17_nfs-server.yaml to 17_nfs-server.sh

---
+++ k8s/k8s-1/assets/17_nfs-server.sh
@@ -0,0 +1,4 @@
+mkdir /exports
+docker run -d --net=host --privileged --name nfs-server -v /exports:/exports /katacoda/contained-nfs-server:centos7 /exports/data-0001 /exports/data-0002
+IP=$(hostname -I | cut -d' ' -f1)
+sed "s/NFS_SERVER_IP/$IP/g" -i /root/17/17_pv.yaml
